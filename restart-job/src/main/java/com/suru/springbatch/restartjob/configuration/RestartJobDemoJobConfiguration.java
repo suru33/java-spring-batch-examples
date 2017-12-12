126cd3046a48899a0c588b1cecd6fade7d9a4c40
SuRu <su33.dev@gmail.com>
2017-12-12 15:30:21 -0600

    restart job example

--- restart-job/src/main/java/com/suru/springbatch/restartjob/configuration/RestartJobDemoJobConfiguration.java
+++ restart-job/src/main/java/com/suru/springbatch/restartjob/configuration/RestartJobDemoJobConfiguration.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
 @Configuration
 public class RestartJobDemoJobConfiguration {
     @Autowired
